5851d044afdafefdcbbee935b94009519a382246
CptIdea <pliyznik@yandex.ru>
2021-07-30 15:51:33 +0300

    метод получения счетчиков онлайна пользователей

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -12,6 +12,7 @@ func ListenAndServe(port string, server *HttpServer) chan error {
 	}
 
 	http.HandleFunc("/getFromTime", server.GetFromTime)
+	http.HandleFunc("/getUserOnlineCounters", server.GetUserOnlineCounters)
 
 	log.Printf("запуск http сервера на порту %q", port)
 
